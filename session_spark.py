0cebd106d7aa625eed5a256cdff279b5acf37ee2
Big Data & Machine Learning <ml.workshop@cnamliban.org>
2020-11-13 08:36:44 +0200

    Update session_spark.py

--- session_spark.py
+++ session_spark.py
@@ -1,5 +1,9 @@
 #Good let test the cration fo a spark session AND context
 import findspark
+import os
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/default-java"
+os.environ["SPARK_HOME"] = "/content/spark-on-collab/spark-3.0.1-bin-hadoop2.7"
+
 findspark.init()
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("BDML").master("local[*]").getOrCreate()
