76bba9a1f39d595b4414c40115cf3e7ab0a54207
Big Data & Machine Learning <ml.workshop@cnamliban.org>
2020-11-13 08:26:30 +0200

    Update init_spark_collab.sh

--- init_spark_collab.sh
+++ init_spark_collab.sh
@@ -2,4 +2,6 @@ apt-get install default-jdk -qq > /dev/null
 [ -f spark-3.0.1-bin-hadoop2.7.tgz ] || wget -q https://mirrors.netix.net/apache/spark/spark-3.0.1/spark-3.0.1-bin-hadoop2.7.tgz
 [ -d spark-3.0.1-bin-hadoop2.7 ] || tar -xzf spark-3.0.1-bin-hadoop2.7.tgz
 pip install -q findspark
-echo "Probably everython ok?"
+echo "Probably everythig ok?"
+export JAVA_HOME="/usr/lib/jvm/default-java"
+export PYTHON_HOME="/content/spark-3.0.1-bin-hadoop2.7"
